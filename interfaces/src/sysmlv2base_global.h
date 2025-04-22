339160683e873816d4c360bd4b8114f974bf5673
Moritz Herzog <herzogm@rptu.de>
2025-04-22 10:06:36 +0200

    Removing Interface folder.

--- interfaces/src/sysmlv2base_global.h
+++
@@ -1,9 +0,0 @@
-#ifdef WIN32
-#ifdef sysmlv2base_EXPORTS
-#define SYSMLV2BASE_EXPORT __declspec(dllexport)
-#else
-#define SYSMLV2BASE_EXPORT __declspec(dllimport)
-#endif
-#else
-#define SYSMLV2BASE_EXPORT
-#endif // WIN32
