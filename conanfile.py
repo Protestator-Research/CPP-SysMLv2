9a1b2dca53b563ab2d9f98c9316e91832bc142fc
Moritz Herzog <moritz.herzog@rptu.de>
2025-11-13 14:53:00 +0100

    Adding interface lib into the conanfile

--- conanfile.py
+++ conanfile.py
@@ -90,7 +90,7 @@ class CPPSysMLRecipe(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["sysmlv2service", "sysmlv2rest", "kerml", "sysmlv2parser"]
+        self.cpp_info.libs = ["sysmlv2interfaces", "sysmlv2service", "sysmlv2rest", "kerml", "sysmlv2parser"]
         self.cpp_info.builddirs.append(os.path.join("lib", "cmake", "sysmlv2"))
 
     
