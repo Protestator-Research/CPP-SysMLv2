81ccb91fcb61dc1aad54fda0a1b6c1ce61e0e29c
Moritz Herzog <moritz.herzog@rptu.de>
2025-11-13 16:05:13 +0100

    ExternalData is added

--- rest-api/include/sysmlv2/rest/entities/ExternalData.h
+++ rest-api/include/sysmlv2/rest/entities/ExternalData.h
@@ -20,6 +20,21 @@
 
 namespace SysMLv2::REST {
     class SYSMLV2REST_EXPORT ExternalData : public Data {
+    public:
+        ExternalData() = delete;
+        explicit ExternalData(std::string jsonStringOrIRI);
+        ~ExternalData() override = default;
+
+        void setResourceIdentifier(std::string resourceIdentifier);
+        std::string resourceIdentifier() const;
+
+        std::string serializeToJson() override;
+
+    protected:
+        void deserializeAndPopulate(const std::string &jsonString) override;
+
+    private:
+        std::string ResourceIdentifier;
 
     };
 } // SysMLv2
--- rest-api/src/sysmlv2/rest/entities/ExternalData.cpp
+++ rest-api/src/sysmlv2/rest/entities/ExternalData.cpp
@@ -2,8 +2,35 @@
 // Created by Moritz Herzog on 27.02.24.
 //
 
+#include <nlohmann/json.hpp>
+#include <sysmlv2/rest/entities/JSONEntities.h>
 #include <sysmlv2/rest/entities/ExternalData.h>
 
 namespace SysMLv2::REST {
+    ExternalData::ExternalData(std::string jsonStringOrIRI) : Data(jsonStringOrIRI){
+        try {
+            ExternalData::deserializeAndPopulate(jsonStringOrIRI);
+        }catch (...) {
+            ResourceIdentifier = jsonStringOrIRI;
+        }
+    }
 
+    void ExternalData::setResourceIdentifier(std::string resourceIdentifier) {
+        ResourceIdentifier = resourceIdentifier;
+    }
+
+    std::string ExternalData::resourceIdentifier() const {
+        return ResourceIdentifier;
+    }
+
+    std::string ExternalData::serializeToJson() {
+        nlohmann::json json = nlohmann::json::parse(Data::serializeToJson());
+        json[JSON_RESOURCE_IDENTIFIER_ENTITY] = ResourceIdentifier;
+        return json.dump(JSON_INTENT);
+    }
+
+    void ExternalData::deserializeAndPopulate(const std::string &jsonString) {
+        nlohmann::json parsedJson = nlohmann::json::parse(jsonString);
+        ResourceIdentifier = parsedJson[JSON_RESOURCE_IDENTIFIER_ENTITY];
+    }
 } // SysMLv2::Entities
